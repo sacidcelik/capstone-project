e8fd11e054bb7e9af9602968cf3e17a2ff96bb1c
Sacid Celik <sacidcelik@gmail.com>
2021-06-15 11:28:48 +0200

    prepared heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 import cors from 'cors';
 import express from 'express';
 import searchRoutes from './routes/search.routes.js';
+import dirname from './lib/pathHelpers.js';
+import path from 'path';
+
+const __dirname = dirname(import.meta.url);
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
-
 server.use(searchRoutes);
 
-server.get('/', (request, response) => response.json({ status: 'alive' }));
+server.use(express.static(path.join(__dirname, '../client/build')));
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
-server.listen(4000);
+server.get('/health', (request, response) =>
+  response.json({ status: 'alive' })
+);
+
+const port = process.env.PORT || 4000;
+server.listen(port);
