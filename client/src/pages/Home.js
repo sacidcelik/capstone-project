3d04b2a6b738e6752b866da8117872eaf2a66129
Sacid Celik <sacidcelik@gmail.com>
2021-06-15 11:56:29 +0200

    heroku-test

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types';
 export default function Home({ onToggleToAndFromLibrary, isInLibrary }) {
   return (
     <main>
-      <h2>Home</h2>
+      <h2>
+        <Homey></Homey>
+      </h2>
       <GlobalSearch
         onToggleToAndFromLibrary={onToggleToAndFromLibrary}
         isInLibrary={isInLibrary}
