525bd8414a1aeb54cd356bd7d2a259062ce1b510
Sacid Celik <sacidcelik@gmail.com>
2021-06-15 17:51:55 +0200

    adding objects to created sehlves, first preview working, shelfconfig divs implemented

--- client/src/components/ShelfCreator.js
+++ client/src/components/ShelfCreator.js
@@ -14,10 +14,10 @@ export default function ShelfCreator() {
     const fieldName = event.target.name;
     let fieldValue = event.target.value;
     if (event.target.id === 'subShelves') {
-      fieldValue = Array.from(
-        { length: Number(event.target.value) },
-        (_, i) => i + 1
-      );
+      fieldValue = [];
+      for (let i = 0; i < event.target.value; i++) {
+        fieldValue.push({ column: i + 1 });
+      }
     }
 
     setShelf({ ...shelf, [fieldName]: fieldValue });
@@ -53,11 +53,63 @@ export default function ShelfCreator() {
           </select>
         </div>
       </ShelfStarter>
-      <section>
+      <ShelfConfigWrapper>
+        {/*         <ShelfConfigHeader>
+          <p>Width</p>
+          <p>Height</p>
+          <p>Compartments</p>
+        </ShelfConfigHeader> */}
+        {shelf.subShelves.map((subShelf, index) => {
+          return (
+            <ShelfConfig>
+              <p>{`Column ${shelf.subShelves[index].column}`}</p>
+              <div>
+                <label htmlFor="width">Width</label>
+                <select name="width" id="width" onChange={updateShelf}>
+                  <option value="">-</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="height">Height</label>
+                <select name="height" id="height" onChange={updateShelf}>
+                  <option value="">-</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="compartments">Compartments</label>
+                <select
+                  name="compartments"
+                  id="compartments"
+                  onChange={updateShelf}
+                >
+                  <option value="">-</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
+            </ShelfConfig>
+          );
+        })}
+      </ShelfConfigWrapper>
+      <ShelfPreview>
         {shelf.subShelves.map((subShelf, index) => {
           return <SubShelf key={index} />;
         })}
-      </section>
+      </ShelfPreview>
     </>
   );
 }
@@ -72,6 +124,70 @@ const ShelfStarter = styled.section`
 
 const SubShelf = styled.div`
   height: 150px;
-  width: 50px;
+  width: 19%;
   border: 1px solid red;
+  margin: 0;
+`;
+
+const ShelfConfigWrapper = styled.section`
+  width: 90%;
+  margin: 1rem auto;
+`;
+
+/* const ShelfConfigHeader = styled.div`
+  grid-column: 2/-1;
+  width: 90%;
+  display: grid;
+  grid-template-columns: auto repeat(3, 1fr);
+
+  p:first-child {
+    justify-self: center;
+    grid-column: 2/3;
+  }
+
+  p:nth-child(2) {
+    justify-self: center;
+    grid-column: 3/4;
+  }
+
+  p:last-child {
+    justify-self: center;
+    grid-column: 4/5;
+  }
+`; */
+
+const ShelfConfig = styled.div`
+  width: 90%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0.5rem auto;
+  padding: 0.5rem;
+  border-radius: var(--border-radius);
+  box-shadow: var(--box-shadow-offset-x) var(--box-shadow-offset-y)
+    var(--box-shadow-blur) var(--box-shadow-color);
+  p {
+    font-size: 0.9rem;
+  }
+
+  label {
+    display: block;
+    font-size: 0.7rem;
+    margin-bottom: 0.2rem;
+  }
+
+  :nth-child(odd) {
+    background: var(--primary);
+  }
+  :nth-child(even) {
+    background: var(--secondary);
+    color: var(--background);
+  }
+`;
+
+const ShelfPreview = styled.section`
+  display: flex;
+  margin: 1rem auto;
+  justify-content: center;
+  align-items: center;
 `;
