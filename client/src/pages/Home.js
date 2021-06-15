fd5cefb5da3cd975a89ca1615b275880ac2e8cff
Sacid Celik <sacidcelik@gmail.com>
2021-06-15 11:59:07 +0200

    fixed heroku error

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,9 +4,7 @@ import PropTypes from 'prop-types';
 export default function Home({ onToggleToAndFromLibrary, isInLibrary }) {
   return (
     <main>
-      <h2>
-        <Homey></Homey>
-      </h2>
+      <h2>Homey</h2>
       <GlobalSearch
         onToggleToAndFromLibrary={onToggleToAndFromLibrary}
         isInLibrary={isInLibrary}
