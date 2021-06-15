0cc16cb1ab4f2443ec2e15552c7a42c74f8195a9
Sacid Celik <sacidcelik@gmail.com>
2021-06-15 12:03:49 +0200

    added dotenv to server.js for heroku port

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import express from 'express';
 import searchRoutes from './routes/search.routes.js';
 import dirname from './lib/pathHelpers.js';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const __dirname = dirname(import.meta.url);
 
