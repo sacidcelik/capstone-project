45157071cdadb64bdb57cc4c9a8b44543cf1967c
Sacid Celik <sacidcelik@gmail.com>
2021-07-01 19:58:50 +0200

    removed unneccessary useEffect import

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components/macro';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import UnreadReadButton from './UnreadReadButton';
 import ShelfEditor from './ShelfEditor';
 import CloseIcon from '../images/closeIcon.svg';
